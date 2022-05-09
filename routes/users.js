11619f4e5593bace3bc4b12c728dcfce224cb7c3
Dylan <d.peate63@gmail.com>
2022-05-09 17:05:33 -0400

    added utils.js, csurf import, get sign-up route

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const csrf = require('csurf');
+const csrfProtection = csrf({ cookie: true });
+const { asyncHandler, handleValidationErrors } = require("../utils");
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -7,8 +11,8 @@ router.get('/', function(req, res, next) {
 });
 
 //signup route
-// router.get('/signup', (req,res,next) => {
-//   res.render('signup', )
-// })
+router.get('/sign-up', csrfProtection, (req, res) => {
+  res.render('sign-up', { csrfToken: req.csrfToken(), errors: [], user: {} })
+})
 
 module.exports = router;
