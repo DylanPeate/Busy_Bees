5afa63fc3ab65fb81fe094813f7e36bdfba889de
Brian <brianshkim25@gmail.com>
2022-05-09 13:50:18 -0700

    users Login/logout auth

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,64 @@
 var express = require('express');
 var router = express.Router();
+const { check, validationResult } = require("express-validator")v
+const cookieParser = require('cookie-parser')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+
+const loginValidators = [
+  check('emailAddress')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Email Address'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Password'),
+];
+
+router.get('/user/login', csrfProtection, (req,res)=>{
+  res.render('user-login', {title: "Log-in", csrfToken: req.csrfToken()})
+})
+
+router.post('/user/login', csrfProtection, loginValidators,
+  asyncHandler(async (req, res) => {
+    const {
+      emailAddress,
+      password,
+    } = req.body;
+
+    let errors = [];
+    const validatorErrors = validationResult(req);
+
+    if (validatorErrors.isEmpty()) {
+      // TODO Attempt to login the user.
+      const user= await User.findOne({where:{emailAddress}})
+      if(user !== null){
+          const passwordMatch = await bcrypt.compare(password, user.hashedPassword.toString())
+          if(passwordMatch){
+              loginUser(req,res,user)
+              return res.redirect('/')
+          }
+      }
+      errors.push('Login failed for the provided email address and password')
+    } else {
+      errors = validatorErrors.array().map((error) => error.msg);
+    }
+
+    res.render('user-login', {
+      title: 'Login',
+      emailAddress,
+      errors,
+      csrfToken: req.csrfToken(),
+    });
+  }));
+
+router.post('/user/logout', (req,res)=>{
+    logoutUser(req,res)
+    res.redirect('/user/login')
+})
+
 
 module.exports = router;
