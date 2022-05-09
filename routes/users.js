1088de6887ea627e7303e0015644472f6b45f229
Brian <brianshkim25@gmail.com>
2022-05-09 13:52:59 -0700

    typo

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const { check, validationResult } = require("express-validator")v
+const { check, validationResult } = require("express-validator")
 const cookieParser = require('cookie-parser')
 
 /* GET users listing. */
