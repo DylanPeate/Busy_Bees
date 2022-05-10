a9dd9b8565e03996269bbf532c0c95f97498bec6
Dylan <d.peate63@gmail.com>
2022-05-10 12:58:32 -0400

    linked app.js with home routes file

---
+++ routes/home.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+const { check, validationResult } = require("express-validator")
+const cookieParser = require('cookie-parser');
+const csrf = require('csurf');
+const csrfProtection = csrf({ cookie: true });
+const { asyncHandler, handleValidationErrors } = require("../utils");
+const bcrypt = require('bcryptjs');
+const db = require('../db/models');
+const { loginUser, logoutUser } = require('../auth');
+
+
+
+
+
+
+module.exports = router;
