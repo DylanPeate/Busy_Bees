7d81e5040ddb6c0af787491b25dd090741165bcb
Dylan <d.peate63@gmail.com>
2022-05-10 13:27:29 -0400

    added new-list get/post routes, new-list.pug file

--- routes/home.js
+++ routes/home.js
@@ -9,9 +9,21 @@ const bcrypt = require('bcryptjs');
 const db = require('../db/models');
 const { loginUser, logoutUser } = require('../auth');
 
+//list routes
+router.get('/new-list', csrfProtection, (req, res) => {
+    res.render('new-list', { title: 'New-list', csrfToken: req.csrfToken() })
+})
 
-
-
+router.post('/new-list', csrfProtection, asyncHandler(async (req, res) => {
+    const user_id = req.session.auth.userId;
+    const { name } = req.body;
+    if (name.length < 1) {
+        req.errors.push('Name is too short')
+    } else {
+        await db.list.create({ name, user_id })
+        res.redirect('/home')
+    }
+}))
 
 
 module.exports = router;
