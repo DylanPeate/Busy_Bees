3ebe263823c459bbfb1057b5c99e41c4daa7e5d9
Dylan <d.peate63@gmail.com>
2022-05-11 16:45:03 -0400

    A LOT

--- routes/home.js
+++ routes/home.js
@@ -9,21 +9,29 @@ const bcrypt = require('bcryptjs');
 const db = require('../db/models');
 const { loginUser, logoutUser, requireAuth } = require('../auth');
 
-router.get('/', requireAuth, async (req, res) => {
+router.get('/', requireAuth, csrfProtection, async (req, res) => {
+    const user_id = req.session.auth.userId;
     const lists = await db.list.findAll({
         where: { user_id: req.session.auth.userId }
     })
-    const tasks = [];
-    lists.forEach(async (list) => {
-        listId = list.id;
-        currentTasks = await db.task.findAll({
-            where: { list_id: listId }
-        })
-        tasks.push(...currentTasks)
+    const mainList = await db.list.findOne({
+        where: { user_id: user_id, name: 'All Tasks' }
     })
-    setTimeout(() => {
-        res.render('home', { lists, tasks })
-    }, 100)
+    const pageId = mainList.id;
+
+    // const tasks = [];
+    // lists.forEach(async (list) => {
+    //     listId = list.id;
+    //     currentTasks = await db.task.findAll({
+    //         where: { list_id: listId }
+    //     })
+    //     tasks.push(...currentTasks)
+    // })
+    // setTimeout(() => {
+    //     res.render('home', { lists, pageId, tasks, crsfToken: req.csrfToken() })
+    // }, 100)
+
+    res.redirect(`/home/list/${pageId}`)
 })
 
 
@@ -45,7 +53,7 @@ router.post('/new-list', csrfProtection, asyncHandler, requireAuth, (async (req,
 }))
 
 
-router.get('/list/:id', requireAuth, (async (req, res) => {
+router.get('/list/:id', csrfProtection, requireAuth, (async (req, res) => {
     //get info
     const lists = await db.list.findAll({
         where: { user_id: req.session.auth.userId }
@@ -54,26 +62,27 @@ router.get('/list/:id', requireAuth, (async (req, res) => {
     const tasks = await db.task.findAll({
         where: { list_id: pageId }
     })
-    res.render('home', { lists, pageId, tasks });
+    res.render('home', { lists, pageId, tasks, csrfToken: req.csrfToken() });
 }))
 
 // tasks
-router.post('/list/:id/new-task', csrfProtection, asyncHandler, (async (req, res) => {
-    const { name, date_due, priority, completed, deleted, notes } = req.body;
-
+router.post('/list/:id/new-task', csrfProtection, asyncHandler(async (req, res, next) => {
+    const { name, date_due, priority, completed, deleted } = req.body;
     //get their main list id
     const user_id = req.session.auth.userId;
     const mainList = await db.list.findOne({
-        where: { userId: user_id, name: 'All Tasks' }
+        where: { user_id, name: 'All Tasks' }
     })
-
+    //ADD CSRF ON ALL /HOME RENDERS
     //check what list
-    const pageId = parseInt(req.params.id, 10);
-    if (pageId === mainList.id) {
+    let pageId = parseInt(req.params.id, 10);
+
+    if (pageId === mainList.id || pageId === NaN) {
         //create task in mainList
         await db.task.create({
             name, date_due, priority, completed, list_id: mainList.id
         })
+        pageId = mainList.id;
     } else {
         //create task in mainlist and list with pageId
         await db.task.create({
@@ -84,10 +93,7 @@ router.post('/list/:id/new-task', csrfProtection, asyncHandler, (async (req, res
         })
     }
 
-    //create task
-    const newTask = await db.task.create({
-        name, priority, completed, deleted, notes, list_id
-    })
+    res.redirect(`/home/list/${pageId}`)
 
 }))
 
