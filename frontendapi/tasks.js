eaeaa42a7c09fb5123d66cfbaa5222c20fa1667e
AnthonyRo1 <anthonyseo556@gmail.com>
2022-05-12 16:59:32 -0700

    updating the code to add front end api functionality

--- frontendapi/tasks.js
+++ frontendapi/tasks.js
@@ -1,19 +1,23 @@
 const express = require('express');
 const router = express.Router();
+
+
+
 const { asyncHandler } = require('../utils')
 const db = require('../db/models');
+const { task } = db;
 const { requireAuth } = require('../auth');
 const csrf = require('csurf');
 const csrfProtection = csrf({ cookie: true });
-const { task } = db;
+
 
 
 // test route handler
-router.get('/tasks', async (req, res) => {
-  const allTasks = await tasks.findAll();
+// router.get('/tasks', async (req, res) => {
+//   const allTasks = await tasks.findAll();
 
-  res.json({ allTasks })
-});
+//   res.json({ allTasks })
+// });
 
 
 // post route handler to submit task data sent from javascripts/index.js
@@ -22,31 +26,45 @@ router.post('/tasks', requireAuth, asyncHandler(async (req, res) => {
   const url = req.headers.referer.split('/');
   const pageId = url[url.length - 1]
   const { name } = req.body;
+    let newTask;
 
   if (pageId === "home") {
-    const newTask = await task.create({
+     newTask = await task.create({
       name,
-      user_id
+      user_id,
     });
   } else {
-    const newTask = await task.create({
+     newTask = await task.create({
       name,
       user_id,
       list_id: pageId
     });
   }
 
+  console.log(newTask.id)
+  return res.json({task: `${newTask.id}`});
+}))
 
-  return res.json({ name });
+
+router.get('/tasks/taskid', csrfProtection, asyncHandler(async(req, res) => {
+  const user_id = req.session.auth.userId;
+  const url = req.headers.referer.split('/');
+  const listId = url[url.length - 1];
+
+  const lists = await db.list.findAll()
+
+  return res.json({lists, csrfToken: req.csrfToken() })
 }))
 
 
 
-module.exports = router;
+router.put('/tasks/edit-task', csrfProtection, asyncHandler(async(req, res) => {
+    console.log(req.body);
+}))
 
 
 // `/home/list/${pageId}/new-task` <--- former action attribute in home page form for tasks
-
+// grab the length of the tasks +1
 
 
 module.exports = router;
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,15 +1,88 @@
 document.addEventListener('DOMContentLoaded', async () => {
     // obtain the elements needed to interact
+
+    const createForm = (csrfToken, lists, editNameVal, taskId) => {
+        console.log(lists)
+
+        const editTaskForm = document.createElement('form');
+        editTaskForm.setAttribute('dataset-taskid', taskId)
+        editTaskForm.setAttribute('id', 'edit-task-form');
+
+
+        const csrfInput = document.createElement('input');
+        csrfInput.setAttribute('type', 'hidden');
+        csrfInput.setAttribute('name', '_csrf');
+        csrfInput.setAttribute('value', csrfToken); // <--- csrfToken pass in 
+
+        const editName = document.createElement('input');
+        editName.setAttribute('type', 'text');
+        editName.setAttribute('name', 'edit-name-box');
+        editName.setAttribute('id', 'edit-name');
+        editName.value = editNameVal;
+
+        // DATE 
+        const dateDiv = document.createElement('div');
+        dateDiv.setAttribute('id', 'date-div');
+        const dateLabel = document.createElement('label');
+        dateLabel.setAttribute('id', 'date-label');
+        dateLabel.setAttribute('for', 'date');
+        dateLabel.innerText = 'Due';
+
+        const dateInput = document.createElement('input');
+        dateInput.setAttribute('type', 'date');
+        dateInput.setAttribute('name', 'date');
+        dateInput.setAttribute('id', 'task-form-date');
+
+        dateDiv.append(dateLabel);
+        dateDiv.append(dateInput);
+        // DATE 
+
+        // New List Select Dropdown 
+        const newListDiv = document.createElement('div');
+        newListDiv.setAttribute('id', 'list-div');
+        const newListLabel = document.createElement('for', 'select-list')
+        newListLabel.setAttribute('id', 'list-label')
+        newListLabel.innerText = 'List'
+        const newListInput = document.createElement('select');
+        newListInput.setAttribute('name', 'select-list')
+        newListInput.setAttribute('id', 'task-form-select')
+
+        newListDiv.appendChild(newListLabel);
+        newListDiv.appendChild(newListInput);
+        // new List Select Dropdown 
+
+        for (let i = 0; i < lists.length; i++) {
+            console.log(lists[i].name)
+            const option = document.createElement('option');
+            option.innerText = lists[i].name;
+            option.setAttribute('value', lists[i].name)
+            newListInput.appendChild(option);
+        }
+
+        const submitButton = document.createElement('input');
+        submitButton.setAttribute('type', 'submit');
+        submitButton.setAttribute('id', 'edit-task-submit');
+
+        editTaskForm.appendChild(editName);
+        editTaskForm.appendChild(csrfInput);
+        editTaskForm.appendChild(dateDiv);
+        editTaskForm.appendChild(newListDiv);
+        editTaskForm.appendChild(submitButton);
+
+        return editTaskForm;
+    }
+
     const newTaskBtn = document.getElementById('new-task-btn');
     const taskContainer = document.querySelector('.tasks-container');
     const taskName = document.getElementById('new-task-input');
+    const eachTask = document.querySelectorAll('.task-single')
+    const listSummary = document.getElementById('list-summary');
+    
 
-
-    // on click, grab info from text box and send post request
-    // to our API
+    // On click of new task button, create new task
+    // without redirecting 
     newTaskBtn.addEventListener('click', async(e) => {
         e.preventDefault();
-    
     const taskElement = document.createElement('p');
     taskElement.innerText = taskName.value;
     taskElement.className = 'task-single'
@@ -26,14 +99,93 @@ document.addEventListener('DOMContentLoaded', async () => {
         headers: {
             "Content-Type": "application/json"
         },
-    });
-    
-    if (!res.ok) {
-        throw res;
-        }
+    }).then(response => response.json())
+    taskElement.setAttribute('id', res.task)
+
+    // Possible: You may need to add a check for res.ok status
     } catch (e) {
        // TODO - throw a custom error 
        console.log(e)
     }
         })  
-})
+
+
+
+
+        let editTaskClicked = false;
+        // On click of task, open edit task menu 
+    taskContainer.addEventListener('click', async(e) => {
+        const taskEle = e.target;
+        if (taskEle.className === 'task-single') {
+            editTaskClicked = true;
+            const taskName = taskEle.innerText;
+            const taskId =  Number(taskEle.getAttribute('id'));
+            
+
+
+            //Create the div form 
+            const editTaskDivHeader = document.createElement('div');
+            editTaskDivHeader.setAttribute('id', 'edit-task-header')
+            editTaskDivHeader.innerText = 'Edit Task'
+            const editTaskDiv = document.createElement('div');
+            editTaskDiv.setAttribute('id', 'edit-task');
+
+            // make fetch request to add the csrfToken
+            const fetchInfo = await fetch('http://localhost:8080/api/tasks/taskid')
+            const data = await fetchInfo.json();
+            
+            const csrfToken = data.csrfToken
+            const lists = data.lists;
+
+            const createdForm = createForm(csrfToken, lists, taskEle.innerText, taskEle.getAttribute('id'))
+
+        
+            
+            editTaskDiv.appendChild(editTaskDivHeader)
+            editTaskDiv.appendChild(createdForm);
+            listSummary.appendChild(editTaskDiv);
+        }
+    })
+
+    // edit-task-submit
+    
+    // task-form-date - DATE 
+    // select-list - LIST 
+    // edit-name-box - NAME 
+    console.log(editTaskClicked)
+    if (editTaskClicked === true) {
+    const submitEditTask = document.getElementById('id', 'edit-task-submit');
+    console.log(submitEditTask);
+    submitEditTask.addEventListener('click', async(e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        const newDate = document.getElementById('task-form-date');
+        const newList = document.getElementById('select-list');
+        const newName = document.getElementById('edit-name-box');
+        const formData = document.getElementById('edit-task-form');
+        const taskId = formData.dataset.taskId;
+        console.log(taskId);
+        const body = {
+            date: newDate.value,
+            list: newList.value,
+            name: newName.value,
+            taskId: taskId
+        }
+        try {
+            await fetch('http://localhost:8080/api/tasks/edit-task', {
+            method: "PUT",
+            body: JSON.stringify(body),
+            header: {
+                "Content-Type": "application/json"
+            }})
+        } catch (e) {
+            console.log(e);
+        }
+            editTaskClicked = false; 
+    })
+}
+
+
+});
+
+
