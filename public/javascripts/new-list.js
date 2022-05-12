202fcf8977ec8193e8d53a32314e8abb6f5c3b6a
dannytoan <dannytoan1@gmail.com>
2022-05-12 13:50:08 -0700

    added popup functionality for confirm delete list

--- public/javascripts/new-list.js
+++ public/javascripts/new-list.js
@@ -1,9 +1,11 @@
 
 document.addEventListener("DOMContentLoaded", (e) => {
     const listBtn = document.querySelector(".list-btn")
+    const deleteBtn = document.getElementById("delete-list-btn")
+    const confirmNoBtn = document.getElementById("no-confirm-btn")
+    const confirmationPopUp = document.getElementById("confirm-delete");
 
     listBtn.addEventListener("click", (e) => {
-        console.log("helloooooooooooooo")
         const form = document.getElementById("list-form-container")
         if (form.classList.contains("hidden")) {
             form.classList.remove("hidden")
@@ -11,4 +13,23 @@ document.addEventListener("DOMContentLoaded", (e) => {
             form.classList.add("hidden")
         }
     })
+
+    deleteBtn.addEventListener("click", (e) => {
+        if (confirmationPopUp.classList.contains("hidden")) {
+            confirmationPopUp.classList.remove("hidden")
+        } else {
+            confirmationPopUp.classList.add("hidden")
+        }
+
+    })
+
+    confirmNoBtn.addEventListener("click", (e) => {
+        if (confirmationPopUp.classList.contains("hidden")) {
+            confirmationPopUp.classList.remove("hidden")
+        } else {
+            confirmationPopUp.classList.add("hidden")
+        }
+
+    })
+
 })
