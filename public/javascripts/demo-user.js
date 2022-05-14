86ebf06f4df0d56f603f68877f1ec5d8dbc8a518
AnthonyRo1 <anthonyseo556@gmail.com>
2022-05-13 20:23:19 -0700

    added some code to start the demo user functionality

--- public/javascripts/demo-user.js
+++ public/javascripts/demo-user.js
@@ -1,9 +1,15 @@
 document.addEventListener('DOMContentLoaded', async() => {
 
+    if (document.referrer === 'http://localhost:8080/') {
+    console.log("DEMO")
+    }
+  let demoEmail = 'demo@email.com';
+  let demoPass = 'demopassword';
 
 
-  
+  //demo@email.com 
+  // demopassword
   const emailInput = document.querySelector('.inputBox-log');
 
-
+  console.log(document.referrer)
 })
